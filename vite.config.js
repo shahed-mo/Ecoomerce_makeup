03216b2bfa63b60658c31eb5ea5472982961849f
Shahed-mo <shahdmohamedsayed123@gmail.com>
2024-09-09 00:18:08 +0300

    Fix mdb-react-ui-kit import issues

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      // Comment this out if mdb-react-ui-kit should be included
+      external: ['mdb-react-ui-kit']
+    }
+  }
+});
